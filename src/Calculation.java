efe20ee81a4ae411bf17f08c168a6584eb211979
Rida-Darwish <71960678+Rida-Darwish@users.noreply.github.com>
2020-10-04 10:10:40 +0200

    First draft before making test directory

---
+++ src/Calculation.java
@@ -0,0 +1,15 @@
+public class Calculation {
+
+    //method that returns the multiple of 2 numebrs
+    public static int multiply(int x, int y){ return x*y; }
+
+    //methof that returns the sum of 2 numbers
+    public static int addition(int x, int y){return x + y;}
+
+    //method that returns the quotient of 2 numebrs
+    public static int divide(int x, int y){ return x/y; }
+
+    //methof that returns the difference of 2 numbers
+    public static int subtact(int x, int y){return x - y;}
+
+}
